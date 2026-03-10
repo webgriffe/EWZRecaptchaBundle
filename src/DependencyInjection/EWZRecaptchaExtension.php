7e7b94895ab7cba6a39a6fd0171f943e9d9bca87
Andrea Zambon <azambon@webgriffe.com>
2026-03-10 14:31:40 +0100

    Imports the latest fixes by gjuric

--- src/DependencyInjection/EWZRecaptchaExtension.php
+++ src/DependencyInjection/EWZRecaptchaExtension.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration.
